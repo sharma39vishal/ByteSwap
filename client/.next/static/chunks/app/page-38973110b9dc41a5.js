9e481f70761ec6779ac805a7bdad4af64aaddc80
sharma39vishal <sharma39vishal@gmail.com>
2023-08-20 21:09:32 +0530

    remove .next

--- client/.next/static/chunks/app/page-38973110b9dc41a5.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6843:function(n,e,t){Promise.resolve().then(t.t.bind(t,3240,23)),Promise.resolve().then(t.t.bind(t,9806,23))},9806:function(){}},function(n){n.O(0,[240,253,961,744],function(){return n(n.s=6843)}),_N_E=n.O()}]);
