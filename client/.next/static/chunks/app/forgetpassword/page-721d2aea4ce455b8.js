d9efe3c05cdbfaee4ae153821e7415301f350dc1
sharma39vishal <sharma39vishal@gmail.com>
2023-11-17 16:28:15 +0530

    package Update

--- client/.next/static/chunks/app/forgetpassword/page-721d2aea4ce455b8.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[572],{5507:function(n,e,u){Promise.resolve().then(u.t.bind(u,5728,23))},5728:function(){}},function(n){n.O(0,[253,961,744],function(){return n(n.s=5507)}),_N_E=n.O()}]);
